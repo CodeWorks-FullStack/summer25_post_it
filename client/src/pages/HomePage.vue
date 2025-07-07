186b413c208a86968b46909f1e00bca5ed9a5bec
Jeremy Fowler <jembyfowler@gmail.com>
2025-07-07 12:39:58 -0600

    little stylin

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,19 @@
-<script setup >
-import Example from '@/components/Example.vue';
+<script setup>
 
 
 </script>
 
 <template>
-  <Example />
+  <!-- TODO make the filter bar -->
+  <section class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="fredoka-font fw-bold border-bottom border-gray my-3">
+          Popular Albums
+        </h2>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
