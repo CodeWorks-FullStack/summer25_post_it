dd17f998eb518aab9b12beb9360a860443de9e9c
Jeremy Fowler <jembyfowler@gmail.com>
2025-07-07 11:23:03 -0600

    registered schema and yeeted value junk

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
-import { ValueSchema } from '../models/Value.js'
+import { AlbumSchema } from '../models/Album.js';
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Albums = mongoose.model('Album', AlbumSchema)
 }
 
 export const dbContext = new DbContext()
